0df25d88989ed4baf17708b92109547accf1c024
Ayush Mishra <46623370+AyushWebDev@users.noreply.github.com>
2021-05-10 21:26:13 +0530

    Add files via upload

---
+++ routes/edu.js
@@ -0,0 +1,29 @@
+const express=require('express');
+const User=require("../model/user");
+const router=express.Router();
+
+router.patch("/:id",async (req,res)=>{
+    try{
+        const id=req.params.id;
+        const updatedData=await User.findByIdAndUpdate({_id: id},
+         {$push:{education: {$each: req.body.data}}},{new: true})
+         res.json(updatedData);
+    }catch(e){
+        res.json(e);
+        console.log(e);
+    }
+})
+
+router.patch("/remove/:id",async (req,res)=>{
+    try{
+        const id=req.params.id;
+        const deletedData=await User.findByIdAndUpdate({_id: id},
+         {$pull:{education: {_id: req.body.id}}},{new: true});
+         res.json(deletedData);
+    }catch(e){
+        res.json(e);
+        console.log(e);
+    }
+})
+
+module.exports=router;
